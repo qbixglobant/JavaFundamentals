b5923032ff9470320bc6424960720cdfe904cfd7
Daniel Gonzalez Briseño <86681538+qbixglobant@users.noreply.github.com>
2021-09-13 10:44:58 -0500

    Primitive Types

---
+++ 01-primitive-types.java
@@ -0,0 +1,23 @@
+// byte | 1 byte | [-128 to 127]
+byte amount = 8;
+
+// short | 2 Bytes | [-32K to 32K]
+short year = 2022;
+
+// integer | 4 Bytes | [-2B to 2B]
+int youtubeViews = 235_485_345;
+
+// long | 8 Bytes | L, l
+long views = 3_252_587_432L;
+
+// float | 4 bytes | F, f
+float price2 = 2.25F;
+
+// double | 8 bytes
+double price3 = 2.253248;
+
+// Boolean | 1 Byte | true, false
+boolean isActive = true;
+
+// char
+char a = 'A';
