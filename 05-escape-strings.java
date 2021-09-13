38a1f7534bcb7a0087f950b6735a08d5b2d8e03c
Daniel Gonzalez Briseño <86681538+qbixglobant@users.noreply.github.com>
2021-09-13 11:26:05 -0500

    Escape Strings

---
+++ 05-escape-strings.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\"Java\"");
+        System.out.println("c:\\Windows\\...");
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\tWorld");
+    }
+
+}
