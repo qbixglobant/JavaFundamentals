241ff371ed7b7aa5f8568be8467d58568361c091
Daniel Gonzalez Briseño <86681538+qbixglobant@users.noreply.github.com>
2021-09-13 11:21:38 -0500

    Update and rename 05-strings.java to 04-strings.java

--- 04-strings.java
+++ 04-strings.java
@@ -5,13 +5,18 @@ public class Main {
         // String message = new String("Hello World");
 
         String message = "Hello World" + "!!";
+        String message2 = "    Java    ";
 
         System.out.println(message);
         System.out.println("Ends with !! " + message.endsWith("!!"));
         System.out.println("Starts with !! " + message.startsWith("!!"));
         System.out.println("Length " + message.length()  + " characters");
-        System.out.println("Index of W " + message.indexOf("W") + " position");
+        System.out.println("Index of W | " + message.indexOf("W") + " position");
         System.out.println("Replace ! with * | " + message.replace("!", "*"));
+        System.out.println("Lowercase | " + message.toLowerCase());
+        System.out.println("Uppercase | " + message.toUpperCase());
+        System.out.println("--" + message2 + "--");
+        System.out.println("Trim | " + "--" + message2.trim() + "--");
     }
 
 }
